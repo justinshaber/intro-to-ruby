69f6c975d33f697d5284ccfa0a162ad08e2bca59
Justin Shaber <justinshaber@gmail.com>
2021-06-09 10:45:36 -0700

    the_basics exercises

---
+++ the_basics/ex3.rb
@@ -0,0 +1,13 @@
+movies = {
+  :Gladiator => 1975,
+  :"Scent_of_a_Woman" => 2004,
+  :Avatar => 2013,
+  :Shrek => 2001,
+  :"Dances_with_Wolves" => 1981
+}
+puts movies[:Gladiator]
+puts movies[:Scent_of_a_Woman]
+puts movies[:Avatar]
+puts movies[:Shrek]
+puts movies[:Dances_with_Wolves]
+
---
+++ the_basics/ex5.rb
@@ -0,0 +1,9 @@
+factor5 = 5 * 4 * 3 * 2
+factor6 = 6 * factor5
+factor7 = 7 * factor6
+factor8 = 8 * factor7
+
+puts factor5
+puts factor6
+puts factor7
+puts factor8
