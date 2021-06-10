8170e803d5aad0e472c5023dda7d3685f6f71587
Justin Shaber <justinshaber@gmail.com>
2021-06-10 13:16:18 -0700

    Exercises for chapter 2

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Hi #{first_name} #{last_name}, I'm Ruby :)"
+
+10.times { puts first_name + " " + last_name }
+
+
