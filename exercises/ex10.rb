12d257244e37e467c53b3aa1de772d198311020e
Justin Shaber <justinshaber@gmail.com>
2021-06-17 23:19:08 -0700

    EEnd of book exercises

---
+++ exercises/ex10.rb
@@ -0,0 +1,12 @@
+hash_arrays = { arr1: [1, 2, 3, 4, 5],
+                arr2: [6, 7, 8, 9, 10]
+              }
+
+p hash_arrays[:arr1]
+
+array_hashes = [{name: "John"}, 
+                {age: 32}, 
+                {birthday: "October 45th"}
+                ]
+
+p array_hashes[1]
